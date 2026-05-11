02f0269214de62c87206bba79163e364d5f69523
kizaru <san.kacharava@gmail.com>
2026-05-11 04:56:27 +0400

    add tests for card component

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -6,9 +6,11 @@ type Props = { pokemon: PokemonDetails }
 class Card extends Component<Props> {
   render() {
     const { name, sprite, flavorText } = this.props.pokemon
+    const spriteSrc = sprite.trim() !== '' ? sprite : undefined
+
     return (
       <article className="pokemon-card">
-        <img className="pokemon-sprite" src={sprite} alt="" />
+        <img className="pokemon-sprite" src={spriteSrc} alt="" />
         <h3 className="pokemon-name">{name}</h3>
         <p className="pokemon-description">{flavorText}</p>
       </article>
